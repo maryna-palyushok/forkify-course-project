15856dabbc5e7db451ca9d20a5bf9f1db544da02
maryna-palyushok <mpaliushok@gmail.com>
2021-03-16 15:45:38 +0200

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -133,11 +133,6 @@ const controlAddRecipe = async function(newRecipe) {
 
 };
 
-const newFeature = function() {
-  console.log('Welcome to the application');
-}
-
-
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
@@ -146,6 +141,5 @@ const init = function() {
   bookmarksView.addHandlerRender(controlBookmarks);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
