1e42b43897cd0caddbdfb596b180d3e863acf34d
maryna-palyushok <mpaliushok@gmail.com>
2021-03-16 13:04:54 +0200

    Added welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -133,6 +133,9 @@ const controlAddRecipe = async function(newRecipe) {
 
 };
 
+const newFeature = function() {
+  console.log('Welcome to the application');
+}
 
 
 const init = function() {
@@ -143,6 +146,6 @@ const init = function() {
   bookmarksView.addHandlerRender(controlBookmarks);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
